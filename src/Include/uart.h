0a860698c5d553a3f558e629abcd4da550b6ca55
kishankrs <kishankrs@outlook.com>
2022-01-26 13:44:51 +0530

    bug fix

--- src/Include/uart.h
+++ src/Include/uart.h
@@ -2,14 +2,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-//define some macros
+//defining macros
 #define BAUD 9600
 #define x (((F_CPU)/(BAUD*16UL))-1)       //define baud
 
 void uart_init (void);
 void uart_Tx_char(unsigned char data);
-void uart_Tx_string(chaar* StringPtr);
+void uart_Tx_string(char* StringPtr);
 unsigned char uart_recieve(void);
-void uart_Tx_number(unit32_t);
-void uart iota(unit32_t n, char s[]);
+void uart_Tx_number(uint32_t);
+void uart_itoa(uint32_t n, char s[]);
 static void reverse(char s[]);
